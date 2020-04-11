c0811e7ed2ba106ead127b882ed42fae8820e546
Tường Nghi <62927599+NgoNguyenTuongNghi@users.noreply.github.com>
2020-04-11 09:38:36 +0700

    Update NhanVien.java

--- BaiTapJava_BaiTap1/src/app/NhanVien.java
+++ BaiTapJava_BaiTap1/src/app/NhanVien.java
@@ -7,31 +7,25 @@ public class NhanVien {
     private double tienLuong;
     private int tongSoGioLam;
 
-
     public void setTen(String ten) {
         this.ten = ten;
     }
-
 
     public void setTuoi(int tuoi) {
         this.tuoi = tuoi;
     }
 
-
     public void setDiaChi(String diaChi) {
         this.diaChi = diaChi;
     }
-
 
     public void setTienLuong(double tienLuong) {
         this.tienLuong = tienLuong;
     }
 
-
     public void setTongSoGioLam(int tongSoGioLam) {
         this.tongSoGioLam = tongSoGioLam;
     }
-
 
     public NhanVien() {
     }
@@ -45,18 +39,13 @@ public class NhanVien {
     }
 
     public double getThuong() {
-        if (tongSoGioLam >= 200) return tienLuong * 0.2;
-        else if (tongSoGioLam >= 100) return tienLuong * 0.1;
+        if (tongSoGioLam >= 200) return tienLuong*0.2;
+        else if (tongSoGioLam >= 100) return tienLuong*0.1;
         else return 0;
     }
 
     public String getThongTin() {
-        return "Ten: " + ten +
-                ", Tuoi: " + tuoi +
-                ", Dia chi: " + diaChi +
-                ", Tien luong: " + tienLuong +
-                ", Tong so gio lam: " + tongSoGioLam +
-                ", Tien thuong: " + getThuong() + ".";
+		return "Ten: " + ten + " -- Tuoi: " + tuoi + " -- Dia chi: " + diaChi + " -- Tien luong: " + tienLuong + 
+			" -- Tong so gio lam: " + tongSoGioLam + " -- Tien thuong: " + getThuong() + ".";
     }
-
 }
