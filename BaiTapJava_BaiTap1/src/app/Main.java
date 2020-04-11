d486a6dfe52899bb05ab0fba582375092a99a541
Tường Nghi <62927599+NgoNguyenTuongNghi@users.noreply.github.com>
2020-04-11 09:38:34 +0700

    Update Main.java

--- BaiTapJava_BaiTap1/src/app/Main.java
+++ BaiTapJava_BaiTap1/src/app/Main.java
@@ -10,19 +10,15 @@ public class Main {
         nv1.setTen("Ngo Nguyen Tuong Nghi");
         nv1.setTuoi(25);
         nv1.setDiaChi("Dien Khanh, Khanh Hoa");
-        nv1.setTienLuong(220000);
-        nv1.setTongSoGioLam(130);
+        nv1.setTienLuong(720000);
+        nv1.setTongSoGioLam(230);
         System.out.println("Thong tin nhan vien:\n" + nv1.getThongTin());
 
-
-        NhanVien nv2 = new NhanVien("Tran Thanh Thi", 22, "Dien Khanh,Khanh Hoa", 200000, 120);
-        System.out.println("Thong tin nhan vien:\n" + nv2.getThongTin());
-
-
         QuanLyNhanVien quanLyNhanVien = new QuanLyNhanVien();
-        NhanVien nv3 = new NhanVien("Le Tan Dat", 21, "Dien Khanh, Khanh Hoa", 200000, 125);
-        NhanVien nv4 = new NhanVien("Diep Tu Thinh", 21, "Nha Trang, Khanh Hoa", 200000, 110);
-        NhanVien nv5 = new NhanVien("Nguyen Viet Trong", 21, "Huong Khe, Ha Tinh", 330000, 105);
+        NhanVien nv2 = new NhanVien("Tran Thanh Thi", 22, "Dien Khanh,Khanh Hoa", 600000, 180);
+        NhanVien nv3 = new NhanVien("Le Tan Dat", 21, "Dien Khanh, Khanh Hoa", 600000, 175);
+        NhanVien nv4 = new NhanVien("Diep Tu Thinh", 21, "Nha Trang, Khanh Hoa", 5600000, 110);
+        NhanVien nv5 = new NhanVien("Nguyen Viet Trong", 21, "Huong Khe, Ha Tinh", 330000, 97);
 
         quanLyNhanVien.them(nv1);
         quanLyNhanVien.them(nv2);
